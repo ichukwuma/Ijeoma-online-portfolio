6b2e315ebd2e1cce36bd85cf802250943badfa68
Ijeoma Chukwuma <99043556+ichukwuma@users.noreply.github.com>
2022-07-07 14:21:50 -0500

    Delete portfolio_project directory

--- portfolio_project/src/index.js
+++
@@ -1,8 +0,0 @@
-const hamburgerBtn = document.getElementById('hamburger')
-const navList = document.getElementById('nav-list')
-
-function toggleBtn(){
-    navList.classList.toggle('show')
-}
-
-hamburgerBtn.addEventListener('click', toggleBtn)
